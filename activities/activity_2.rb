9f0795f751517264817c6af87bd8e9c850b9acd8
markcgarjas <markcarlo859@gmail.com>
2022-09-16 16:44:04 +0800

    Creating a activity file 2

--- activities/activity_2.rb
+++ activities/activity_2.rb
@@ -3,3 +3,22 @@
 #  Values will be your respective names.
 #  Write a function that takes a Hash and returns the full name.
 #  Print on console the result.
+
+def fullname
+
+  name = ""
+
+
+  my_fullname = {first_name: "Mark Carlo", middle_name: "Viloria", lastname: "Garjas" }
+  
+  my_fullname.each do |key, value|
+    name = name + value
+    if key.to_s != "lastname"
+      name = name + " "
+    end
+  end
+  return name
+end
+p fullname
+
+
